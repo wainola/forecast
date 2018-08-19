5b160dd01dc7e66f87fcf30727e0d70ce5874eab
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-08-19 19:19:23 -0300

    saving error on redis and getting data from the api

--- handlers/darkSkyHandler.js
+++ handlers/darkSkyHandler.js
@@ -1,20 +1,24 @@
 const axios = require('axios');
+const moment = require('moment');
+const redis = require('redis');
+
+const client = redis.createClient()
 
 const darkSkyHandler = async (socket, DEV_KEY) => {
-  console.log('devkey', DEV_KEY)
   try {
     if (Math.random(0, 1) < 0.1) {
-
-      throw new Error('Ohp! there is an error with the API');
+      console.error('Error and timestamp', moment().format())
+      client.hset(`forecast-${moment().format()}`, 'There was an error with the API', moment().format(), redis.print)
+      throw new Error('Ops! there is an error with the API');
     }
-    // const santiago = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
-    // const zurich = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
+    const santiago = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/-33.447487,-70.673676`);
+    const zurich = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/47.451542,8.564572`);
     // const auckland = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`)
     // const sydney = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
     // const london = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
     // const georgia = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
-    console.log('res from employees', res.data)
-    socket.emit('FROM_API', res.data)
+    console.log('res data', santiago.data)
+    socket.emit('FROM_API', [santiago.data, zurich.data])
   } catch (err) {
     console.error(`Error: ${err}`)
   }
