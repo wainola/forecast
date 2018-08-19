91ebf24eb3eb8ddb4c94594f70ba1daf01b7f762
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-08-19 18:52:42 -0300

    error

--- handlers/darkSkyHandler.js
+++ handlers/darkSkyHandler.js
@@ -3,7 +3,16 @@ const axios = require('axios');
 const darkSkyHandler = async (socket, DEV_KEY) => {
   console.log('devkey', DEV_KEY)
   try {
-    const res = await axios.get(`http://localhost:9002/employees`);
+    if (Math.random(0, 1) < 0.1) {
+
+      throw new Error('Ohp! there is an error with the API');
+    }
+    // const santiago = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
+    // const zurich = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
+    // const auckland = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`)
+    // const sydney = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
+    // const london = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
+    // const georgia = await axios.get(`https://api.darksky.net/forecast/${devKey}/43.7695,11.2558`);
     console.log('res from employees', res.data)
     socket.emit('FROM_API', res.data)
   } catch (err) {
