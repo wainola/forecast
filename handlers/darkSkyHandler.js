c68d0441a574908922f9638484641ee5253bb98c
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-08-19 20:33:25 -0300

    retreiving data without imperials units

--- handlers/darkSkyHandler.js
+++ handlers/darkSkyHandler.js
@@ -11,12 +11,12 @@ const darkSkyHandler = async (socket, DEV_KEY) => {
       client.hset(`forecast-${moment().format()}`, 'There was an error with the API', moment().format(), redis.print)
       throw new Error('Ops! there is an error with the API');
     }
-    const santiago = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/-33.447487,-70.673676`);
-    const zurich = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/47.451542,8.564572`);
-    const auckland = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/-36.848461,174.763336`)
-    const sydney = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/-33.865143,151.209900`);
-    const london = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/51.509865,-0.118092`);
-    const georgia = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/33.247875,-83.441162`);
+    const santiago = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/-33.447487,-70.673676?units=si`);
+    const zurich = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/47.451542,8.564572?units=si`);
+    const auckland = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/-36.848461,174.763336?units=si`)
+    const sydney = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/-33.865143,151.209900?units=si`);
+    const london = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/51.509865,-0.118092?units=si`);
+    const georgia = await axios.get(`https://api.darksky.net/forecast/${DEV_KEY}/33.247875,-83.441162?units=si`);
     console.log('res data', santiago.data)
     socket.emit('FROM_API', [santiago.data, zurich.data, auckland.data, sydney.data, london.data, georgia.data])
   } catch (err) {
